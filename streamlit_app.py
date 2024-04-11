4ee11482eeecffec53e06ee0eff3a064d1af92a5
Jyoti S Nayak <125571567+jyoti-sn@users.noreply.github.com>
2024-04-11 12:38:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,16 @@
-pip install plotly
 import streamlit as st
-# Choose your visualization library (here, using Plotly)
 import pandas as pd
-import plotly.express as px
 
-
+# Load the data into a Pandas DataFrame (assuming you have a 'data.csv' file)
 df = read_csv('https://github.com/jyoti-sn/NSS_NLP/blob/main/NSS_Country.csv')
 
+# Dashboard Header and Layout
 st.title('Country Counts Dashboard')
-
-# Selectbox for year selection
 selected_year = st.selectbox('Select a year:', df['Year'].unique())
 
 # Filter data based on selected year
 filtered_df = df[df['Year'] == selected_year]
 
-fig = px.bar(filtered_df, x='Country', y='Count')
+# Display the bar chart
+st.bar_chart(filtered_df, x='Country', y='Count')
 
-# Display the plot
-st.plotly_chart(fig)
